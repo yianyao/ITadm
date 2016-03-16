42a2a6d4d31019fda27d9cc11dfb3b514664482c
yianyao <yianyao@live.cn>
2016-03-16 18:01:18 +0800

    服务相关
    异常类

---
+++ com/itpdc/service/DbException.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yianyao
+ * Date: 16-3-16
+ * Time: 上午9:10
+ */
+
+namespace com\itpdc\service;
+
+
+class DbException extends \Exception
+{
+
+} 
---
+++ com/itpdc/service/LoadException.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yianyao
+ * Date: 16-3-15
+ * Time: 下午8:22
+ */
+
+namespace com\itpdc\service;
+
+
+class LoadException extends \Exception
+{
+
+} 
